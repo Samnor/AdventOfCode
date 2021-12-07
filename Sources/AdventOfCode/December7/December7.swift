f8959126fd1f9212c33a7b00a5c1bcf40c827513
Samuel Norling <samuel.norling@qbtech.com>
2021-12-07 18:26:31 +0100

    December7: part two done

--- Sources/AdventOfCode/December7/December7.swift
+++ Sources/AdventOfCode/December7/December7.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum December7 {
-    static func main() {
+    static func mainOne() {
         let lines = DecemberIO.getLines()
         let positions = lines
             .first!
@@ -21,6 +21,31 @@ enum December7 {
         for position in (0...maxPos) {
             let distSum = positions.reduce(0) { partialResult, crabPosition in
                 return partialResult + abs(position - crabPosition)
+            }
+            minSum = min(minSum, distSum)
+        }
+        print(minSum)
+    }
+
+    static func posDistance(position: Int, crabPosition: Int) -> Int {
+        let distance = abs(position - crabPosition)
+        let arithmeticSeriesSum = distance * (1 + distance)/2
+        return arithmeticSeriesSum
+    }
+
+    static func main() {
+        let lines = DecemberIO.getLines()
+        let positions = lines
+            .first!
+            .split(separator: ",")
+            .map(String.init)
+            .compactMap(Int.init)
+        print(positions)
+        let maxPos = positions.max()!
+        var minSum = 999999999
+        for position in (0...maxPos) {
+            let distSum = positions.reduce(0) { partialResult, crabPosition in
+                return partialResult + posDistance(position: position, crabPosition: crabPosition)
             }
             minSum = min(minSum, distSum)
         }
